5351bdb05f693c28a1e8471ceb68ce4434e9516f
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-19 14:15:23 +0900

    fix: [남현기] allowedOrigins 수정 [CorsConfig]

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080") // 보안에서 vue는 허용하겠다.
                 .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://3.36.11.166/:80")
+                .allowedOrigins("http://3.36.11.166:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
